1f4476d7f141aef1a874a749ab1568511853d09b
Nathan Hardy <nhardy@live.com.au>
2017-09-09 10:28:37 +1200

    Initial boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# etude-07
-COSC326 Étude 7
+# Étude 7
+
+Java solution for COSC326 Étude 7 - Joined Up Writing
+
+See http://www.cs.otago.ac.nz/cosc326/PDF/7-Writing.pdf for more details
+
+## Usage
---
+++ src/JoinUp.java
@@ -0,0 +1,18 @@
+/**
+ * Etude 7 - Joined Up Writing
+ * @author Kimberley Louw, Nathan Hardy
+ */
+
+public class JoinUp {
+    public static void main(String[] args) {
+        new JoinUp().start();
+    }
+
+    public JoinUp(){
+
+    }
+
+    public void start() {
+        System.out.println("Working");
+    }
+}
