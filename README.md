6f7cc9f3b664c144e3bcec88123685d6020c3c57
Nathan Hardy <nhardy@live.com.au>
2017-09-09 10:56:34 +1200

    Load inputs

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Java solution for COSC326 Étude 7 - Joined Up Writing
 See http://www.cs.otago.ac.nz/cosc326/PDF/7-Writing.pdf for more details
 
 ## Usage
+
+```
+cd src
+javac JoinUp.java
+java JoinUp suffix read < ../dict.txt
+```
--- src/JoinUp.java
+++ src/JoinUp.java
@@ -1,18 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Etude 7 - Joined Up Writing
  * @author Kimberley Louw, Nathan Hardy
  */
 
 public class JoinUp {
+    private String first;
+    private String last;
+    private ArrayList<String> dictionary = new ArrayList<String>();
+
     public static void main(String[] args) {
-        new JoinUp().start();
+        new JoinUp(args[0], args[1]).run();
     }
 
-    public JoinUp(){
+    public JoinUp(String first, String last) {
+        this.first = first;
+        this.last = last;
 
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            dictionary.add(scanner.nextLine());
+        }
+        scanner.close();
     }
 
-    public void start() {
-        System.out.println("Working");
+    public void run() {
+        System.out.println("Word: " + dictionary.get(0));
     }
 }
